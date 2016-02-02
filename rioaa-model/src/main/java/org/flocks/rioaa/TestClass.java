28687a5037aed56a77ed88ca59e40ec5b19c6a5c
Midhun Jose <midhun.jose@cgi.com>
2016-02-02 14:29:38 +0530

    project structure basic

---
+++ rioaa-model/src/main/java/org/flocks/rioaa/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.flocks.rioaa;
+
+/**
+ * @author midhun.jose
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
